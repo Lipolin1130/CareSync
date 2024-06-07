a9fdfb03d3998af7d84564c5e4bebdbaa4c1d737
Funghi1130 <funghi1130@gmail.com>
2024-06-07 16:31:14 +0800

    Initial Commit

---
+++ CareSync/CareSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareSyncApp.swift
+//  CareSync
+//
+//  Created by Mac on 2024/6/7.
+//
+
+import SwiftUI
+
+@main
+struct CareSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
